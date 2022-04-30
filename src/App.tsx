a143ce93ee0bd06fd3ece647860e711f38665eba
Joao Guimaraes <psjoaovictor@gmail.com>
2022-04-30 11:13:23 -0300

    configurando globalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import styled from "styled-components";
+import { GlobalStyle } from "./style/global";
 
-const Title = styled.h1`
-  font-size: 2rem;
-  color: #8257e6;
-`
-
-export function App(){
-  return <Title>Hello Word</Title>
+export function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      <h1>Hello Word</h1>
+    </div>
+  );
 }
