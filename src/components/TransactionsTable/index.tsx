3127497cc4b5d584897231a05015a1b60a3b36cb
Joao Guimaraes <psjoaovictor@gmail.com>
2022-05-01 15:25:18 -0300

    criando transacao a partir da api

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,11 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../../services/api";
 import { Container } from "./styles";
 
+interface Transactions {
+  id: string;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+}
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transactions[]>([]);
+
   useEffect(() => {
-    api.get("transactions")
-      .then((response) => console.log(response.data));
+    api.get("transactions").then((response) => setTransactions(response.data.transactions));
   }, []);
 
   return (
@@ -20,18 +30,14 @@ export function TransactionsTable() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>22/04/2022</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">-R$2.000</td>
-            <td>Desenvolvimento</td>
-            <td>22/04/2022</td>
-          </tr>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>{transaction.amount}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.createdAt}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,46 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { createServer }from "miragejs";
+import { createServer, Model }from "miragejs";
 import {App} from "./App"
 
 createServer({
-  routes(){
-    this.namespace = "api";
-    this.get("/transactions", () => {
-      return [
+  models:{
+    transaction: Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
         {
-          id: 1,
-          title: "transactions",
-          amount: 400,
+          id:1,
+          title: "Freelance de website",
           type: "deposit",
-          category: "Food",
-          createdAt: new Date()
+          category: "Dev",
+          amount: 5000,
+          createdAt: new Date("2022-02-12 09:00:00")
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Moradia",
+          amount: 1000,
+          createdAt: new Date("2022-02-12 10:00:00")
         }
       ]
     })
+  },
+
+  routes(){
+    this.namespace = "api";
+    this.get("/transactions", () => {
+      return this.schema.all("transaction")
+    })
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return schema.create("transaction", data);
+    });
   }
 })
 
